45b64b6573749bd402c9be9ee32b6dea5540555f
crushjunior <charushnikovvadim@gmail.con>
2023-03-02 15:14:52 +0300

    Пример перехвата и обработки исключения в блоке try-catch метода. ver. 2.0

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,19 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         int a = 1;
-        if (a < 0) {
             try {
-                throw new IOException();
+                doSmth(a);
             } catch (IOException e) {
                 System.out.println(e.getMessage());
             } finally {
                 System.out.println("Независимо от того что произошлов в try и catch, будет выведена в консоль эта запись");
             }
+
+    }
+
+    public static void doSmth(int a) throws IOException {
+        if (a < 0) {
+            throw new IOException("Меньше нуля");
         }
     }
 }
